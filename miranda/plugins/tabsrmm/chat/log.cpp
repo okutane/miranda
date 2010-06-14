okutane/miranda

commit d46817e978b331bf6d3d7ac5e99e2a42e38883e5

various memory leaks in tabSRMM
git-svn-id: http://miranda.googlecode.com/svn/trunk@11981 83df23c2-51fd-11de-bb24-2d5e9b4af41d

miranda/plugins/tabsrmm/include/themes.h
@@ -260,6 +260,7 @@ public:
 	void			Init(bool fStartup = false);
 	void			Load();
 	void			Unload();
+	void			UnloadTabs();
 	void			setFileName();
 	void			ReadItem(const int id, const TCHAR *section);
 	void			LoadItems();

commit 9a4ca6158f22167821ae259d6ee5fbf6ab881a89

* task bar tabs (Win7) - fixed default value for the option * another tool bar rendering fix (for classic Windows theme only) * fixed a regression introduced by r11981 (possible crash after unloading a skin) * task bar tabs - different method of activating the tab/window * task bar tabs - do not depend on aero (the tabs w/o preview should work without aero being active).

miranda/plugins/tabsrmm/chat/log.cpp
@@ -800,14 +800,6 @@ static char* Log_CreateRTF(LOGSTREAMDATA *streamData)
 			if (lin->next != NULL)
 				Log_Append(&buffer, &bufferEnd, &bufferAlloced, "\\par ");
 
-			/* TODO IEView support
-			if (streamData->dat->hwndIEView != 0 || streamData->dat->hwndHPP != 0) {
-				LogEventIEView(streamData, lin->ptszNick);
-				streamData->lin = lin;
-				lin = lin->prev;
-				continue;
-			}
-			*/
 			if (streamData->dat->dwFlags & MWF_DIVIDERWANTED || lin->dwFlags & MWF_DIVIDERWANTED) {
 				static char szStyle_div[128] = "\0";
 				if (szStyle_div[0] == 0)

miranda/plugins/tabsrmm/include/taskbar.h
@@ -31,6 +31,8 @@
  * - Windows 7 taskbar integration class
  * - Proxy window class, needed to support custom aero peek tab
  *   thumbnails
+ * - Thumbnail classes to provide task bar thumbnails for Aero peek
+ *   preview.
  */
 
 #ifndef __TASKBAR_H
@@ -46,10 +48,10 @@ public:
 	CThumbBase				(const CProxyWindow* pWnd);
 	virtual ~CThumbBase		();
 
-	const HBITMAP			getHBM() const { return(m_hbmThumb); }
-	const bool				isValid() const { return(m_isValid); }
-	virtual void			setValid(const bool fNewValid) { m_isValid = fNewValid; }
-	virtual void			update() = 0;
+	const HBITMAP			getHBM				() const { return(m_hbmThumb); }
+	const bool				isValid				() const { return(m_isValid); }
+	virtual void			setValid			(const bool fNewValid) { m_isValid = fNewValid; }
+	virtual void			update				() = 0;
 
 protected:
 	HBITMAP					m_hbmThumb, m_hbmOld;
@@ -63,10 +65,10 @@ protected:
 	LONG					m_cx, m_cy;
 	HFONT					m_hOldFont;
 
-	virtual void			renderBase();
+	virtual void			renderBase			();
 
 private:
-	virtual void			renderContent() = 0;
+	virtual void			renderContent		() = 0;
 
 private:
 	bool					m_isValid;
@@ -77,10 +79,10 @@ class CThumbIM : public CThumbBase {
 public:
 	CThumbIM				(const CProxyWindow* pWnd);
 	virtual ~CThumbIM		() {};
-	void					update();
+	void					update				();
 
 private:
-	void					renderContent();
+	void					renderContent		();
 };
 
 class CThumbMUC : public CThumbBase {
@@ -88,10 +90,10 @@ class CThumbMUC : public CThumbBase {
 public:
 	CThumbMUC				(const CProxyWindow* pWnd);
 	virtual ~CThumbMUC		() {};
-	void					update();
+	void					update				();
 
 private:
-	void					renderContent();
+	void					renderContent		();
 };
 
 class CProxyWindow
@@ -100,29 +102,31 @@ public:
 	CProxyWindow(const TWindowData *dat);
 	~CProxyWindow();
 
-	void					updateIcon(const HICON hIcon) const;
-	void					updateTitle(const TCHAR *tszTitle) const;
-	void					setBigIcon(const HICON hIcon, bool fInvalidate = true);
-	void					activateTab() const;
-	void					Invalidate() const;
-	const TWindowData*		getDat() const { return(m_dat); }
-	const LONG				getWidth() const { return(m_width); }
-	const LONG				getHeight() const { return(m_height); }
-	const HWND				getHwnd() const { return(m_hwnd); }
-	const HICON				getBigIcon() const { return(m_hBigIcon); }
+	void					updateIcon			(const HICON hIcon) const;
+	void					updateTitle			(const TCHAR *tszTitle) const;
+	void					setBigIcon			(const HICON hIcon, bool fInvalidate = true);
+	void					activateTab			() const;
+	void					Invalidate			() const;
+	const TWindowData*		getDat				() const { return(m_dat); }
+	const LONG				getWidth			() const { return(m_width); }
+	const LONG				getHeight			() const { return(m_height); }
+	const HWND				getHwnd				() const { return(m_hwnd); }
+	const HICON				getBigIcon			() const { return(m_hBigIcon); }
+	void					verifyDwmState		();
 
-	static	LRESULT CALLBACK stubWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-	static	void			add(TWindowData *dat);
-	static 	void			verify(TWindowData *dat);
+	static	LRESULT CALLBACK stubWndProc		(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+	static	void			add					(TWindowData *dat);
+	static 	void			verify				(TWindowData *dat);
 
 private:
 	const TWindowData*		m_dat;
 	HWND					m_hwnd;
 	LONG					m_width, m_height;
 	HICON					m_hBigIcon;
-	LRESULT CALLBACK		wndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-	void 					sendThumb(LONG width, LONG height);
-	void					sendPreview();
+
+	LRESULT CALLBACK		wndProc				(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+	void 					sendThumb			(LONG width, LONG height);
+	void					sendPreview			();
 	CThumbBase*				m_thumb;
 };
 

miranda/plugins/tabsrmm/include/themes.h
@@ -24,7 +24,7 @@
  *
  * part of tabSRMM messaging plugin for Miranda.
  *
- * (C) 2005-2009 by silvercircle _at_ gmail _dot_ com and contributors
+ * (C) 2005-2010 by silvercircle _at_ gmail _dot_ com and contributors
  *
  * $Id$
  *
@@ -260,7 +260,7 @@ public:
 	void			Init(bool fStartup = false);
 	void			Load();
 	void			Unload();
-	void			UnloadTabs();
+	void			UnloadAeroTabs();
 	void			setFileName();
 	void			ReadItem(const int id, const TCHAR *section);
 	void			LoadItems();
@@ -302,7 +302,7 @@ public:
 	static UINT TSAPI		DrawRichEditFrame(HWND hwnd, const TWindowData *mwdat, UINT skinID, UINT msg, WPARAM wParam, LPARAM lParam, WNDPROC OldWndProc);
 	static UINT TSAPI		NcCalcRichEditFrame(HWND hwnd, const TWindowData *mwdat, UINT skinID, UINT msg, WPARAM wParam, LPARAM lParam, WNDPROC OldWndProc);
 	static HBITMAP TSAPI 	CreateAeroCompatibleBitmap(const RECT &rc, HDC dc);
-	static int TSAPI		RenderText(HDC hdc, HANDLE hTheme, const TCHAR *szText, RECT *rc, DWORD dtFlags, const int iGlowSize = DEFAULT_GLOW_SIZE, COLORREF clr = 0);
+	static int TSAPI		RenderText(HDC hdc, HANDLE hTheme, const TCHAR *szText, RECT *rc, DWORD dtFlags, const int iGlowSize = DEFAULT_GLOW_SIZE, COLORREF clr = 0, bool fForceAero = false);
 	static void TSAPI		MapClientToParent(HWND hwndClient, HWND hwndParent, RECT &rc);
 	static void TSAPI		RenderToolbarBG(const TWindowData *dat, HDC hdc, const RECT &rcWindow);
 	static HBITMAP TSAPI	ResizeBitmap(HBITMAP hBmpSrc, LONG width, LONG height, bool &mustFree);

miranda/plugins/tabsrmm/include/translator.h
@@ -276,8 +276,10 @@ public:
 		GEN_TOOLTIP_DONTADD										= 227,
 		GEN_TOOLTIP_EXPANDSIDEBAR								= 228,
 		GEN_TASKBAR_STRING_CHAT_ROOM							= 229,
-		GEN_STRING_SERVER_WINDOW								= 230,
-		STR_LAST 												= 231
+		GEN_TASKBAR_STRING_SERVER_WINDOW						= 230,
+		GEN_TASKBAR_STRING_UNREAD								= 231,
+		GEN_TASKBAR_STRING_USERS								= 232,
+		STR_LAST 												= 233
 	};
 
 	enum {

miranda/plugins/tabsrmm/include/version.h
@@ -1,5 +1,5 @@
-#define VER_PLAIN	"3,0,0,35"
+#define VER_PLAIN	"3,0,0,36"
 #define _VER_MAJOR	3
 #define _VER_MINOR  0
 #define _VER_REVISION 0
-#define _VER_BUILD	35
+#define _VER_BUILD	36

miranda/plugins/tabsrmm/resource.rc
@@ -10,10 +10,8 @@
 #include <windows.h>
 #include <winres.h>
 #include "../../include/statusmodes.h"
-
-
-
-
+
+
 /////////////////////////////////////////////////////////////////////////////
 #undef APSTUDIO_READONLY_SYMBOLS
 
@@ -200,12 +198,10 @@ BEGIN
     CONTROL         "Only show tab bar when it's needed",IDC_O_NOTABS,"Button",BS_AUTOCHECKBOX | NOT WS_VISIBLE | WS_TABSTOP,105,65,233,10
     CONTROL         "Container stays on top",IDC_O_STICKY,"Button",BS_AUTOCHECKBOX | NOT WS_VISIBLE | WS_TABSTOP,105,77,233,10
     CONTROL         "Vertical maximize",IDC_VERTICALMAX,"Button",BS_AUTOCHECKBOX | NOT WS_VISIBLE | WS_TABSTOP,105,88,233,10
-
     LTEXT           "Close/hide inactive container after",IDC_O_AUTOHIDE,105,103,122,11
     EDITTEXT        IDC_AUTOCLOSETABTIME,240,101,41,12,ES_AUTOHSCROLL | ES_NUMBER
     CONTROL         "",IDC_AUTOCLOSETABSPIN,"msctls_updown32",UDS_SETBUDDYINT | UDS_ALIGNRIGHT | UDS_AUTOBUDDY | UDS_ARROWKEYS,155,171,11,11
     LTEXT           "sec. (0=never)",IDC_O_AUTOHIDESECONDS,290,103,122,11
-
     CONTROL         "Automatically size input area (*)",IDC_AUTOSPLITTER,
                     "Button",BS_AUTOCHECKBOX | NOT WS_VISIBLE | WS_TABSTOP,105,120,233,10
     CONTROL         "Use default setting",IDC_O_FLASHDEFAULT,"Button",BS_AUTORADIOBUTTON | WS_GROUP,105,64,233,11
@@ -259,7 +255,8 @@ BEGIN
     COMBOBOX        IDC_AVATARMODE,105,72,220,14,CBS_DROPDOWNLIST | WS_VSCROLL | WS_TABSTOP
     LTEXT           "Show my own avatars",IDC_O_STATIC_OWNAVATAR,105,88,233,12
     COMBOBOX        IDC_OWNAVATARMODE,105,98,220,14,CBS_DROPDOWNLIST | WS_VSCROLL | WS_TABSTOP
-    CONTROL         "Show avatars on task bar (Win 7, large task bar required)",IDC_AVATARSONTASKBAR,"Button",BS_AUTOCHECKBOX | WS_TABSTOP,105,120,233,30
+    CONTROL         "Show avatars on task bar (Win 7, large task bar required)",IDC_AVATARSONTASKBAR,
+                    "Button",BS_AUTOCHECKBOX | WS_TABSTOP,105,120,233,30
     CONTROL         "Enable sound effects",IDC_O_ENABLESOUNDS,"Button",BS_AUTOCHECKBOX | WS_TABSTOP,105,65,233,10
     CONTROL         "Play sounds when minimized",IDC_O_SOUNDSMINIMIZED,
                     "Button",BS_AUTOCHECKBOX | WS_TABSTOP,105,77,233,10
@@ -569,29 +566,29 @@ BEGIN
     PUSHBUTTON      "Cancel",IDCANCEL,201,296,71,16
 END
 
-IDD_USERPREFS1 DIALOG 0, 0, 286, 265
+IDD_USERPREFS1 DIALOGEX 0, 0, 286, 265
 STYLE DS_SETFONT | DS_FIXEDSYS | DS_CONTROL | WS_CHILD | WS_CAPTION
 CAPTION "Dialog"
-FONT 8, "MS Shell Dlg"
+FONT 8, "MS Shell Dlg", 0, 0, 0x0
 BEGIN
-    GROUPBOX        "Icons",IDC_STATIC,8,36,266,42
+    GROUPBOX        "Icons",IDC_STATIC,8,36,266,57
     CONTROL         "Event type icons in the message log",IDC_UPREFS_SHOWICONS,
-                    "Button",BS_AUTO3STATE | WS_TABSTOP,15,47,121,11
+                    "Button",BS_AUTO3STATE | WS_TABSTOP,15,47,252,11
     CONTROL         "Text symbols as event markers",IDC_UPREFS_SHOWSYMBOLS,
-                    "Button",BS_AUTO3STATE | WS_TABSTOP,147,47,121,11
+                    "Button",BS_AUTO3STATE | WS_TABSTOP,15,63,247,11
     CONTROL         "Use different icons to mark incoming and outgoing messages",IDC_UPREFS_INOUTICONS,
-                    "Button",BS_AUTO3STATE | WS_TABSTOP,15,60,244,11
-    GROUPBOX        "Date and time",IDC_STATIC,8,86,266,45
-    CONTROL         "Show timestamps",IDC_UPREFS_SHOWTIMESTAMP,"Button",BS_AUTO3STATE | WS_TABSTOP,15,97,121,11
+                    "Button",BS_AUTO3STATE | WS_TABSTOP,15,79,252,11
+    GROUPBOX        "Date and time",IDC_STATIC,8,96,266,45
+    CONTROL         "Show timestamps",IDC_UPREFS_SHOWTIMESTAMP,"Button",BS_AUTO3STATE | WS_TABSTOP,15,107,121,11
     CONTROL         "Show seconds in timestamp",IDC_UPREFS_SHOWSECONDS,
-                    "Button",BS_AUTO3STATE | WS_TABSTOP,15,110,121,11
-    CONTROL         "Show date in timestamps",IDC_UPREFS_SHOWDATES,"Button",BS_AUTO3STATE | WS_TABSTOP,147,97,121,11
-    CONTROL         "Use contacts local time",IDC_UPREFS_LOCALTIME,"Button",BS_AUTO3STATE | WS_TABSTOP,147,110,121,11
-    GROUPBOX        "Format",IDC_STATIC,8,132,266,53
-    CONTROL         "Indent message body",IDC_UPREFS_INDENT,"Button",BS_AUTO3STATE | WS_TABSTOP,15,142,121,11
-    CONTROL         "Display grid lines",IDC_UPREFS_GRID,"Button",BS_AUTO3STATE | WS_TABSTOP,15,155,121,11
-    CONTROL         "Message grouping",IDC_UPREFS_GROUPING,"Button",BS_AUTO3STATE | WS_TABSTOP,147,142,121,11
-    CONTROL         "Support BBCODE",IDC_UPREFS_BBCODE,"Button",BS_AUTO3STATE | WS_TABSTOP,147,155,121,11
+                    "Button",BS_AUTO3STATE | WS_TABSTOP,15,120,121,11
+    CONTROL         "Show date in timestamps",IDC_UPREFS_SHOWDATES,"Button",BS_AUTO3STATE | WS_TABSTOP,147,107,121,11
+    CONTROL         "Use contacts local time",IDC_UPREFS_LOCALTIME,"Button",BS_AUTO3STATE | WS_TABSTOP,147,120,121,11
+    GROUPBOX        "Format",IDC_STATIC,8,144,266,42
+    CONTROL         "Indent message body",IDC_UPREFS_INDENT,"Button",BS_AUTO3STATE | WS_TABSTOP,15,154,121,11
+    CONTROL         "Display grid lines",IDC_UPREFS_GRID,"Button",BS_AUTO3STATE | WS_TABSTOP,15,167,121,11
+    CONTROL         "Message grouping",IDC_UPREFS_GROUPING,"Button",BS_AUTO3STATE | WS_TABSTOP,147,154,121,11
+    CONTROL         "Support BBCODE",IDC_UPREFS_BBCODE,"Button",BS_AUTO3STATE | WS_TABSTOP,147,167,121,11
     GROUPBOX        "Misc",IDC_STATIC,8,186,266,52
     CONTROL         "RTL is default text direction",IDC_UPREFS_RTL,"Button",BS_AUTO3STATE | WS_TABSTOP,15,196,248,11
     CONTROL         "Log status changes (in open message windows only)",IDC_UPREFS_LOGSTATUS,
@@ -1088,8 +1085,7 @@ END
 IDR_SKIN_AERO           SKIN_GLYPH              "res\\SKIN\\tabskin_aero.png"
 IDR_SKIN_AERO_GLOW      SKIN_GLYPH              "res\\SKIN\\tabskin_aero_glow.png"
 IDR_SKIN_AERO_SWITCHBAR SKIN_GLYPH              "res\\SKIN\\tabskin_aero_button.png"
-IDR_SKIN_LOGO			SKIN_GLYPH				"res\\SKIN\\unknown.png"
-
+IDR_SKIN_LOGO           SKIN_GLYPH              "res\\SKIN\\unknown.png"
 #endif    // English (United States) resources
 /////////////////////////////////////////////////////////////////////////////
 

miranda/plugins/tabsrmm/src/commonheaders.h
@@ -24,7 +24,7 @@
  *
  * part of tabSRMM messaging plugin for Miranda.
  *
- * (C) 2005-2009 by silvercircle _at_ gmail _dot_ com and contributors
+ * (C) 2005-2010 by silvercircle _at_ gmail _dot_ com and contributors
  *
  * $Id$
  *

miranda/plugins/tabsrmm/src/msgdialog.cpp
@@ -3808,10 +3808,6 @@ quote_from_last:
 						 * the hwndOwner is set to 0 because the window handle is now no longer valid.
 						 * Response for such a job is still silently handled by AckMessage() (sendqueue.c)
 						 */
-						/* TODO
-						 * User may receive NO feedback for sending errors after the session window
-						 * had been closed, since such jobs will be silently discarded.
-						 */
 						if (jobs[i].iStatus == (unsigned)SendQueue::SQ_INPROGRESS)
 							jobs[i].hwndOwner = 0;
 					}

miranda/plugins/tabsrmm/src/sendlater.cpp
@@ -736,7 +736,7 @@ void CSendLater::qMgrSetupColumns()
 		sscanf(szColDefault, szColFormat, &nWidths[0], &nWidths[1], &nWidths[2], &nWidths[3], &nWidths[4]);
 
 	col.mask = LVCF_TEXT|LVCF_WIDTH|LVCF_SUBITEM;
-	col.cx = max(nWidths[0], 10);      // width TODO save/restore user-defined values
+	col.cx = max(nWidths[0], 10);
 	col.pszText = const_cast<TCHAR *>(CTranslator::get(CTranslator::GEN_CONTACT));
 
 	::SendMessage(m_hwndList, LVM_INSERTCOLUMN, 0, reinterpret_cast<LPARAM>(&col));

miranda/plugins/tabsrmm/src/srmm.cpp
@@ -132,6 +132,7 @@ extern "C" int __declspec(dllexport) Unload(void)
 	Chat_Unload();
 	int iRet = SplitmsgShutdown();
 	Skin->setupTabCloseBitmap(true);
+	Skin->UnloadAeroTabs();
 	delete Skin;
 	delete sendLater;
 	delete sendQueue;

miranda/plugins/tabsrmm/src/taskbar.cpp
@@ -34,7 +34,6 @@
  * - custom taskbar thumbnails for aero peek in tabbed containers
  * - read Windows 7 task bar configuration from the registry.
  */
-
 /**
  * how it works:
  *
@@ -101,6 +100,9 @@ bool CTaskbarInteract::haveLargeIcons()
 			::RegQueryValueEx(hKey, _T("TaskbarSmallIcons"), 0, &dwType, (LPBYTE)&val, &size);
 			size = 4;
 			dwType = REG_DWORD;
+			/*
+			 * this is the "grouping mode" setting for the task bar. 0 = always combine, no labels
+			 */
 			::RegQueryValueEx(hKey, _T("TaskbarGlomLevel"), 0, &dwType, (LPBYTE)&valGrouping, &size);
 			::RegCloseKey(hKey);
 		}
@@ -174,11 +176,12 @@ void CTaskbarInteract::SetTabActive(const HWND hwndTab, const HWND hwndGroup) co
  * per session and not outside of WM_INITDIALOG after most things are initialized.
  * @param dat		session window data
  *
- * static member function
+ * static member function. Ignored when OS is not Windows 7 or global option for
+ * Windows 7 task bar support is diabled.
  */
 void CProxyWindow::add(TWindowData *dat)
 {
-	if(IsWinVer7Plus() && PluginConfig.m_useAeroPeek && M->isDwmActive())
+	if(PluginConfig.m_bIsWin7 && PluginConfig.m_useAeroPeek)
 		dat->pWnd = new CProxyWindow(dat);
 	else
 		dat->pWnd = 0;
@@ -194,7 +197,7 @@ void CProxyWindow::add(TWindowData *dat)
  */
 void CProxyWindow::verify(TWindowData *dat)
 {
-	if(M->isDwmActive()) {
+	if(PluginConfig.m_bIsWin7) {
 		if(0 == dat->pWnd) {
 			dat->pWnd = new CProxyWindow(dat);
 			if(dat->pWnd) {
@@ -202,7 +205,12 @@ void CProxyWindow::verify(TWindowData *dat)
 				dat->pWnd->updateTitle(dat->cache->getNick());
 			}
 		}
+		else
+			dat->pWnd->verifyDwmState();
 	}
+	/*
+	 * this should not happens, but who knows...
+	 */
 	else {
 		if(dat->pWnd) {
 			delete dat->pWnd;
@@ -250,6 +258,27 @@ CProxyWindow::~CProxyWindow()
 	if(m_thumb) {
 		delete m_thumb;
 		m_thumb = 0;
+	}
+}
+
+/**
+ * verify status of DWM when system broadcasts a WM_DWMCOMPOSITIONCHANGED message
+ * delete thumbnails, if no longer needed
+ */
+void CProxyWindow::verifyDwmState()
+{
+	if(!M->isDwmActive()) {
+		if(m_thumb) {
+			delete m_thumb;
+			m_thumb = 0;
+		}
+	}
+	else {
+		/*
+		 * force thumbnail recreation
+		 */
+		m_width = 0;
+		m_height = 0;
 	}
 }
 
@@ -302,7 +331,7 @@ void CProxyWindow::sendPreview()
 		 * a minimized container has a null rect as client area, so do not use it
 		 * use the last known client area size instead.
 		 */
-		if(!IsIconic(m_dat->pContainer->hwnd))
+		if(!::IsIconic(m_dat->pContainer->hwnd))
 			::GetClientRect(m_dat->pContainer->hwnd, &rcContainer);
 		else
 			rcContainer = m_dat->pContainer->rcSaved;
@@ -449,9 +478,11 @@ LRESULT CALLBACK CProxyWindow::wndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARA
 			 * to the real message window.
 			 */
 		case WM_ACTIVATE:
-			::SendMessage(m_dat->hwnd, DM_ACTIVATEME, 0, 0);
-			::SetFocus(m_dat->hwnd);
-			return(0);			// no default processing, avoid flickering.
+			if(WA_ACTIVE == wParam) {
+				::PostMessage(m_dat->hwnd, DM_ACTIVATEME, 0, 0);
+				return(0);			// no default processing, avoid flickering.
+			}
+			break;
 
 		case WM_NCDESTROY:
 			::SetWindowLongPtr(hWnd, GWLP_USERDATA, 0);
@@ -542,9 +573,6 @@ void CThumbBase::renderBase()
 	m_hOldFont = reinterpret_cast<HFONT>(::SelectObject(m_hdc, CInfoPanel::m_ipConfig.hFonts[IPFONTID_STATUS]));
 	::GetTextExtentPoint32A(m_hdc, "A", 1, &m_sz);
 
-	if(CSkin::m_pCurrentAeroEffect)
-		m_dtFlags |= (CSkin::m_pCurrentAeroEffect->m_glowSize ? 0x80000000 : 0);
-
 	InflateRect(&m_rc, -3, -3);
 
 	m_rcTop = m_rc;
@@ -577,13 +605,13 @@ void CThumbBase::renderBase()
 	::DrawIconEx(m_hdc, m_rcIcon.right / 2 - lIconSize / 2, m_rcIcon.top, hIcon, lIconSize, lIconSize, 0, 0, DI_NORMAL);
 
 	m_rcIcon.top += (lIconSize + 3);
-	CSkin::RenderText(m_hdc, m_dat->hThemeToolbar, m_dat->szStatus, &m_rcIcon, m_dtFlags | DT_CENTER | DT_WORD_ELLIPSIS, 10);
+	CSkin::RenderText(m_hdc, m_dat->hTheme, m_dat->szStatus, &m_rcIcon, m_dtFlags | DT_CENTER | DT_WORD_ELLIPSIS, 10, 0, true);
 	if(m_dat->dwUnread) {
 		TCHAR	tszTemp[30];
 
 		m_rcIcon.top += m_sz.cy;
-		mir_sntprintf(tszTemp, 30, _T("%d Unread"), m_dat->dwUnread);
-		CSkin::RenderText(m_hdc, m_dat->hThemeToolbar, tszTemp, &m_rcIcon, m_dtFlags | DT_CENTER | DT_WORD_ELLIPSIS, 10);
+		mir_sntprintf(tszTemp, 30, CTranslator::get(CTranslator::GEN_TASKBAR_STRING_UNREAD), m_dat->dwUnread);
+		CSkin::RenderText(m_hdc, m_dat->hTheme, tszTemp, &m_rcIcon, m_dtFlags | DT_CENTER | DT_WORD_ELLIPSIS, 10, 0, true);
 	}
 	m_rcIcon= m_rcTop;
 	m_rcIcon.top += 2;
@@ -679,11 +707,11 @@ void CThumbIM::renderContent()
 	if(0 == (tszStatusMsg = m_dat->cache->getStatusMsg()))
 			tszStatusMsg = CTranslator::get(CTranslator::GEN_NO_STATUS);
 
-	CSkin::RenderText(m_hdc, m_dat->hThemeToolbar, tszStatusMsg, &m_rcBottom, DT_WORD_ELLIPSIS | DT_END_ELLIPSIS | m_dtFlags, 10);
+	CSkin::RenderText(m_hdc, m_dat->hTheme, tszStatusMsg, &m_rcBottom, DT_WORD_ELLIPSIS | DT_END_ELLIPSIS | m_dtFlags, 10, 0, true);
 	m_rcBottom.bottom = m_rc.bottom;
 	m_rcBottom.top = m_rcBottom.bottom - m_sz.cy - 2;
-	CSkin::RenderText(m_hdc, m_dat->hThemeToolbar, Win7Taskbar->haveAlwaysGroupingMode() ? m_dat->cache->getUIN() : m_dat->cache->getNick(),
-					  &m_rcBottom, m_dtFlags | DT_SINGLELINE | DT_WORD_ELLIPSIS | DT_END_ELLIPSIS, 10);
+	CSkin::RenderText(m_hdc, m_dat->hTheme, Win7Taskbar->haveAlwaysGroupingMode() ? m_dat->cache->getUIN() : m_dat->cache->getNick(),
+					  &m_rcBottom, m_dtFlags | DT_SINGLELINE | DT_WORD_ELLIPSIS | DT_END_ELLIPSIS, 10, 0, true);
 
 	if(m_hOldFont)
 		::SelectObject(m_hdc, m_hOldFont);
@@ -720,9 +748,9 @@ void CThumbMUC::update()
 void CThumbMUC::renderContent()
 {
 	if(m_dat->si) {
-		MODULEINFO*		mi = MM_FindModule(m_dat->si->pszModule);
-		TCHAR			tszTemp[250];
-		const TCHAR*	tszStatusMsg = 0;
+		const MODULEINFO*	mi = MM_FindModule(m_dat->si->pszModule);
+		TCHAR				tszTemp[250];
+		const TCHAR*		tszStatusMsg = 0;
 
 		if(mi) {
 			if(m_dat->si->iType != GCW_SERVER) {
@@ -738,17 +766,17 @@ void CThumbMUC::renderContent()
 				}
 				else
 					mir_sntprintf(tszTemp, SIZEOF(tszTemp), CTranslator::get(CTranslator::GEN_TASKBAR_STRING_CHAT_ROOM), _T(""));
-				CSkin::RenderText(m_hdc, m_dat->hThemeToolbar, tszTemp, &m_rcIcon, m_dtFlags | DT_SINGLELINE | DT_RIGHT, 10);
+				CSkin::RenderText(m_hdc, m_dat->hTheme, tszTemp, &m_rcIcon, m_dtFlags | DT_SINGLELINE | DT_RIGHT, 10, 0, true);
 				m_rcIcon.top += m_sz.cy;
-				mir_sntprintf(tszTemp, SIZEOF(tszTemp), _T("%d users"), m_dat->si->nUsersInNicklist);
-				CSkin::RenderText(m_hdc, m_dat->hThemeToolbar, tszTemp, &m_rcIcon, m_dtFlags | DT_SINGLELINE | DT_RIGHT, 10);
+				mir_sntprintf(tszTemp, SIZEOF(tszTemp), CTranslator::get(CTranslator::GEN_TASKBAR_STRING_USERS), m_dat->si->nUsersInNicklist);
+				CSkin::RenderText(m_hdc, m_dat->hTheme, tszTemp, &m_rcIcon, m_dtFlags | DT_SINGLELINE | DT_RIGHT, 10, 0, true);
 			}
 			else {
-				mir_sntprintf(tszTemp, SIZEOF(tszTemp), CTranslator::get(CTranslator::GEN_STRING_SERVER_WINDOW));
-				CSkin::RenderText(m_hdc, m_dat->hThemeToolbar, tszTemp, &m_rcIcon, m_dtFlags | DT_SINGLELINE | DT_RIGHT, 10);
+				mir_sntprintf(tszTemp, SIZEOF(tszTemp), CTranslator::get(CTranslator::GEN_TASKBAR_STRING_SERVER_WINDOW));
+				CSkin::RenderText(m_hdc, m_dat->hTheme, tszTemp, &m_rcIcon, m_dtFlags | DT_SINGLELINE | DT_RIGHT, 10, 0, true);
 				if(mi->tszIdleMsg[0] && _tcslen(mi->tszIdleMsg) > 2) {
 					m_rcIcon.top += m_sz.cy;
-					CSkin::RenderText(m_hdc, m_dat->hThemeToolbar, &mi->tszIdleMsg[2], &m_rcIcon, m_dtFlags | DT_SINGLELINE | DT_RIGHT, 10);
+					CSkin::RenderText(m_hdc, m_dat->hTheme, &mi->tszIdleMsg[2], &m_rcIcon, m_dtFlags | DT_SINGLELINE | DT_RIGHT, 10, 0, true);
 				}
 			}
 		}
@@ -767,7 +795,7 @@ void CThumbMUC::renderContent()
 			tszStatusMsg = tszTemp;
 		}
 
-		CSkin::RenderText(m_hdc, m_dat->hThemeToolbar, tszStatusMsg, &m_rcBottom, DT_WORD_ELLIPSIS | DT_END_ELLIPSIS | m_dtFlags, 10);
+		CSkin::RenderText(m_hdc, m_dat->hTheme, tszStatusMsg, &m_rcBottom, DT_WORD_ELLIPSIS | DT_END_ELLIPSIS | m_dtFlags, 10, 0, true);
 	}
 	/*
 	 * finalize it

miranda/plugins/tabsrmm/src/themes.cpp
@@ -160,7 +160,7 @@ AeroEffect  CSkin::m_aeroEffects[AERO_EFFECT_LAST] = {
 		0,
 		CORNER_ALL,
 		GRADIENT_TB + 1,
-		8,
+		6,
 		0,
 		0x0,
 		0xf0f0f0,
@@ -1127,13 +1127,11 @@ bool CSkin::warnToClose() const
 }
 
 /**
- * Unload the skin. Free everything allocated.
- * Called when:
- * * user unloads the skin from the dialog box
- * * a new skin is loaded by user's request.
+ * free the aero tab bitmaps
+ * only called on exit, NOT when a skin is unloaded as these elements
+ * are always needed (even without a skin)
  */
-
-void CSkin::UnloadTabs()
+void CSkin::UnloadAeroTabs()
 {
 	if(m_tabTop) {
 		delete m_tabTop;
@@ -1160,6 +1158,13 @@ void CSkin::UnloadTabs()
 		m_switchBarItem = NULL;
 	}
 }
+
+/**
+ * Unload the skin. Free everything allocated.
+ * Called when:
+ * * user unloads the skin from the dialog box
+ * * a new skin is loaded by user's request.
+ */
 
 void CSkin::Unload()
 {
@@ -1221,8 +1226,6 @@ void CSkin::Unload()
 	m_ContainerColorKey = 0;
 	m_ContainerColorKeyBrush = m_MenuBGBrush = 0;
 	m_skinEnabled = m_frameSkins = false;
-
-	UnloadTabs();
 
 	if(m_closeIcon)
 		::DestroyIcon(m_closeIcon);
@@ -1402,7 +1405,7 @@ void CSkin::ReadImageItem(const TCHAR *itemname)
 		delete szImageFileName;
 }
 
-/* 				DISABLED TODO (fix it)
+/* 				DISABLED code
 
 void CSkin::ReadButtonItem(const TCHAR *itemName) const
 {
@@ -1953,7 +1956,7 @@ void CSkin::setupAeroSkins()
 	TCHAR	tszFilename[MAX_PATH], tszBasePath[MAX_PATH];
 
 	M->getAeroState();
-	UnloadTabs();
+	UnloadAeroTabs();
 
 	BOOL	isOpaque;
 	HBITMAP hbm;
@@ -2099,9 +2102,6 @@ void CSkin::setupAeroSkins()
 
 	m_tabGlowBottom->setAlphaFormat(AC_SRC_ALPHA, 255);
 	m_tabGlowBottom->setMetrics(bm.bmWidth, bm.bmHeight);
-
-	if(m_switchBarItem)
-		delete m_switchBarItem;
 
 	/*
 	 * background item for the button switch bar
@@ -2407,14 +2407,13 @@ DWORD __fastcall CSkin::HexStringToLong(const TCHAR *szSource)
  * @return
  */
 
-int CSkin::RenderText(HDC hdc, HANDLE hTheme, const TCHAR *szText, RECT *rc, DWORD dtFlags, const int iGlowSize, COLORREF clr)
+int CSkin::RenderText(HDC hdc, HANDLE hTheme, const TCHAR *szText, RECT *rc, DWORD dtFlags, const int iGlowSize, COLORREF clr, bool fForceAero)
 {
 #if defined(_UNICODE)
-	if(PluginConfig.m_bIsVista && !CSkin::m_skinEnabled && hTheme) {
+	if((PluginConfig.m_bIsVista && !CSkin::m_skinEnabled && hTheme) || fForceAero) {
 		DTTOPTS dto = {0};
 		dto.dwSize = sizeof(dto);
-		if(iGlowSize && (M->isAero() || dtFlags & 0x80000000)) {
-			dtFlags &= 0x7fffffff;
+		if(iGlowSize && (M->isAero() || fForceAero)) {
 			dto.iGlowSize = iGlowSize;
 			dto.dwFlags = DTT_COMPOSITED|DTT_GLOWSIZE;
 		}
@@ -2629,9 +2628,9 @@ void CSkin::RenderToolbarBG(const TWindowData *dat, HDC hdc, const RECT &rcWindo
 		else {
 			dat->pContainer->bTBRenderingMode = (M->isVSThemed() ? 1 : 0);
 			m_tmp_tb_high = PluginConfig.m_tbBackgroundHigh ? PluginConfig.m_tbBackgroundHigh :
-					(m_pCurrentAeroEffect ? m_pCurrentAeroEffect->m_clrToolbar : ::GetSysColor(COLOR_3DFACE));
+					((fAero && m_pCurrentAeroEffect) ? m_pCurrentAeroEffect->m_clrToolbar : ::GetSysColor(COLOR_3DFACE));
 			m_tmp_tb_low = PluginConfig.m_tbBackgroundLow ? PluginConfig.m_tbBackgroundLow :
-					(m_pCurrentAeroEffect ? m_pCurrentAeroEffect->m_clrToolbar2 : ::GetSysColor(COLOR_3DFACE));
+					((fAero && m_pCurrentAeroEffect) ? m_pCurrentAeroEffect->m_clrToolbar2 : ::GetSysColor(COLOR_3DFACE));
 
 			bAlphaOffset = PluginConfig.m_tbBackgroundHigh ? 40 : 0;
 			::DrawAlpha(dat->pContainer->cachedToolbarDC, &rcCachedToolbar, m_tmp_tb_high, 55 + bAlphaOffset, m_tmp_tb_low, 0, 9, 0, 0, 0);

miranda/plugins/tabsrmm/src/translator.cpp
@@ -311,7 +311,9 @@ TCHAR* CTranslator::m_strings[STR_LAST] = {
 	 * task bar support (thumbnails)
 	 */
 	LPGENT("Chat room %s"),																/* GEN_TASKBAR_STRING_CHAT_ROOM */
-	LPGENT("Server window"),															/* GEN_STRING_SERVER_WINDOW */
+	LPGENT("Server window"),															/* GEN_TASKBAR_STRING_SERVER_WINDOW */
+	LPGENT("% Unread"),																	/* GEN_TASKBAR_STRING_UNREAD */
+	LPGENT("% User(s)"),																/* GEN_TASKBAR_STRING_USERS */
 };
 
 /*
@@ -439,9 +441,9 @@ TOptionListItem CTranslator::m_lvItemsModPlus[] = {
 	0, LPGENT("Enable icon pack version check (*)"), 1, LOI_TYPE_SETTING, (UINT_PTR)"adv_IconpackWarning", 0,
 	0, LPGENT("Disable error popups on sending failures"), 0, LOI_TYPE_SETTING, (UINT_PTR)"adv_noErrorPopups", 1,
 	0, LPGENT("Use Aero Glass for the message window (Vista+)"), 1, LOI_TYPE_SETTING, (UINT_PTR)"useAero", 0,
-	0, LPGENT("Use Aero Peek features for tabbed Windows (Win7+) (*)"), 0, LOI_TYPE_SETTING, (UINT_PTR)"useAeroPeek", 0,
+	0, LPGENT("Use Windows 7 task bar features for tabbed windows (*)"), 1, LOI_TYPE_SETTING, (UINT_PTR)"useAeroPeek", 0,
 	0, LPGENT("Automatic keyboard layout: Do not load the system default for new contacts"), 1, LOI_TYPE_SETTING, (UINT_PTR)"adv_leaveKeyboardAlone", 0,
-	0, LPGENT("Enable unattended send (experimental, required for multisend and send later, possibly buggy) (*)"), 0, LOI_TYPE_SETTING, (UINT_PTR)"sendLaterAvail", 0,
+	0, LPGENT("Enable unattended send (experimental feature, required for multisend and send later) (*)"), 0, LOI_TYPE_SETTING, (UINT_PTR)"sendLaterAvail", 0,
 	0, NULL, 0, 0, 0, 0
 };
 

miranda/plugins/tabsrmm/tabsrmm_10.vcxproj
@@ -931,6 +931,7 @@
     <None Include="res\SKIN\tabskin_aero.png" />
     <None Include="res\SKIN\tabskin_aero_button.png" />
     <None Include="res\SKIN\tabskin_aero_glow.png" />
+    <None Include="res\SKIN\unknown.png" />
     <None Include="tabmodplus\icons\imgclose.ico" />
     <None Include="tabmodplus\icons\imgopen.ico" />
     <None Include="res\in.ico" />

miranda/plugins/tabsrmm/tabsrmm_10.vcxproj.filters
@@ -678,5 +678,6 @@
     <None Include="chat\Icons\window.ico">
       <Filter>Resource Files\Icons</Filter>
     </None>
+    <None Include="res\SKIN\unknown.png" />
   </ItemGroup>
 </Project>